66b638089f07375c54a02c06563027fcc993ad7e
Raj Thakur <rajthakur0213@gmail.com>
2024-01-27 10:27:02 +0530

    Final commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// popup method to display while the index page.
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        2000 
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
